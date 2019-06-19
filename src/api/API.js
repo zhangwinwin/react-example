da667b0458ef8fc8ff72a3dfd586b7340e8c86a3
apple <apple@appledeMacBook-Pro-2.local>
2019-06-19 16:10:50 +0800

    feat: 增加公共样式和header组件

--- src/api/API.js
+++ src/api/API.js
@@ -2,6 +2,27 @@ import Request from '../units/request'
 
 class API extends Request {
   /**
+   * uploadImage
+   */
+  async uploadImg (params = {}) {
+    try {
+      let result = await this.axios('post', '//', params)
+      if (result && result.status === 1) {
+        return result
+      } else {
+        let err = {
+          tip: '上传图片失败',
+          response: result,
+          data: params,
+          url: ''
+        }
+        throw err
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+  /**
    * 获取记录数据
    * @method get
    * @return {promise}
@@ -15,6 +36,50 @@ class API extends Request {
         let err ={
           response: result,
           data: params,
+          url: ''
+        }
+        throw err
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+
+  /**
+   * 获取商品数据
+   */
+  async getProduction (params = {}) {
+    try {
+      let result = await this.axios('get', '', params)
+      if (result && (result.data instanceof Object) && result.http_code === 200) {
+        return result.data.data || []
+      } else {
+        let err = {
+          tip: '获取商品失败',
+          data: params,
+          response: result,
+          url: ''
+        }
+        throw err
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+
+  /**
+   * 获取佣金数据
+   */
+  async getBalance (params = {}) {
+    try {
+      let result = await this.axios('get', '', params) 
+      if (result && (result.data instanceof Object) && result.http_code === 200) {
+        return result.data.data || []
+      } else {
+        let err = {
+          tip: '获取佣金数据失败',
+          response: params,
+          data: result,
           url: ''
         }
         throw err
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,18 +1,114 @@
 import react, { Component } from 'react'
-import propTypes from 'prop-types'
+import PropTypes from 'prop-types'
+import API from '@/api/API'
+import { connect } from 'react-redux'
+import { is, fromJS } from 'immutable'
+import env from '@/env'
 
 class Home extends Component {
   static propTypes = {
-    formData: propTypes.object.isRequired,
+    formData: PropTypes.object.isRequired,
     saveFormData: PropTypes.func.isRequired,
+    saveImg: PropTypes.func.isRequired,
     clearData: PropTypes.func.isRequired,
-    clearSelected: PropTypes.func.isRequired
+    clearSelected: PropTypes.func.isRequired,
   }
 
+  /**
+   * isToast：是否弹窗
+   * TipText：弹窗显示的文字
+   */
   state = {
     isToast: false,
     TipText: ''
   }
 
+  /**
+   * 已选择商品列表
+   */
   selectedList = []
+
+  handleInput = (type, event) => {
+    let value = event.target.value
+    switch (type) {
+      case 'orderSum':
+        value = value.replace(/\D/g, '')
+      break
+      case 'name':
+      break
+      case 'phoneNo':
+        value = this.padStr(value.replace(/\D/g, ''), [3, 7], ' ', event.target)
+      break
+      default:
+    }
+    this.props.saveFormData(value, type)
+  }
+
+  uploadImage = async event => {
+    try {
+      let formdata = new FormData()
+      formdata.append('file', event.target.files[0])
+      let result = await API.uploadImg({data: formdata})
+      this.props.saveImg(env.imgUrl + result.image_path)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  // 提交表单
+  sumitForm = () => {
+    const {orderSum, name, phoneNo} = this.props.formData
+    let alertTip = ''
+    if(!orderSum.toString().length){
+      alertTip = '请填写金额'
+    }else if(!name.toString().length){
+      alertTip = '请填写姓名'
+    }else if(!phoneNo.toString().length){
+      alertTip = '请填写正确的手机号'
+    }else{
+      alertTip = '添加数据成功'
+      this.props.clearSelected()
+      this.props.clearData()
+    }
+    this.setState({
+      isToast: true,
+      TipText: alertTip,
+    })
+  }
+  
+  // 关闭弹款
+  closeAlert = () => {
+    this.setState({
+      isToast: false,
+      TipText: '',
+    })
+  }
+  
+  // 初始化数据，获取已选择的商品
+  initData = props => {
+    this.selectedProList = []
+    props.proData.dataList.forEach(item => {
+      if(item.selectStatus && item.selectNum){
+        this.selectedProList.push(item)
+      }
+    })
+  }
+  componentWillReceiveProps(nextProps){
+    if(!is(fromJS(this.props.proData), fromJS(nextProps.proData))){
+      this.initData(nextProps)
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
+  }
+
+  componentWillMount(){
+    this.initData(this.props)
+  }
+  render () {
+    return (
+      <main className="home-container">
+      </main>
+    )
+  }
 }
